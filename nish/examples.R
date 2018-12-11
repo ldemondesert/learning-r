318284ea3c95b4e1183dc3129d2434ae4611cf1f
Nishant <nish.kishore@gmail.com>
2018-12-10 22:22:23 -0500

    some updates

--- nish/examples.R
+++ nish/examples.R
@@ -1,1 +1,16 @@
 #Nishant example code
+#numbers
+x <- 1
+#chacters
+c <- "test"
+
+add <- function(a,b){
+  return(a+b)
+}
+
+#Euler Practice 
+
+sum((1:10000)[(1:10000 %% 3 ==0 | 1:10000 %% 5 == 0)])
+
+a <- c(17:21)
+
